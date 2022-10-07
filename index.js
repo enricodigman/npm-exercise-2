932f33dca73cb32d381cac142b1047a825d9c56b
Enrico Digman <essdigman@gmail.com>
2022-10-07 08:05:41 +0800

    Added if-else function

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ let count = 0;
 const joinArray = arguments.join(" ");
 
 for (let letter of joinArray.toLowerCase()) {
-    const vowels = ["a", "e", "i", "o", "u"];
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
     if (vowels.includes(letter)) {
         count++;
     }
 }
 
-console.log(`There ${arguments.length >  1 ? 'are' : 'is'} ${arguments.length === 0 ? '' : arguments.length} command-line argument${arguments.length === 1 ? '' : 's'} passed.`);
-console.log(`There ${count === 1 ? 'is' : 'are'} ${count} ${count === 1 ? 'vowel' : 'vowels'} found.`);
+if (count !== 0 || arguments.length > 0) {
+    console.log(`There ${arguments.length >  1 ? 'are' : 'is'} ${arguments.length} command-line argument${arguments.length === 1 ? '' : 's'} passed.`);
+    console.log(`There ${count === 1 ? 'is' : 'are'} ${count} ${count === 1 ? 'vowel' : 'vowels'} found.`);
+}
+else {
+    console.log('No command-line arguments are passed.');
+}
